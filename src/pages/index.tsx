41326c9cb7c4bd28c84b52c06fee7d1655a3405b
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-11-14 19:45:35 +0100

    Update src/pages/index.tsx from CMS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,33 @@
-import { FC } from "react";
 
-const HomePage: FC = () => {
-	return (
-		<>
-			<div className="bg-white w-full h-full text-black">
-				Test Pierwszego deploymentu
-			</div>
-		</>
-	);
-};
+      import React from 'react';
 
-export default HomePage;
+      const GeneratedPage: React.FC = () => {
+        return (
+          <>
+            <div className="bg-[#9b3636] p-[20px] h-[545px] flex flex-row">
+        <div className="bg-[#955050] p-[20px] w-[567px] h-[234px] flex flex-column">
+        <div className="bg-[#f0f0f0] p-[20px] flex flex-column">
+        
+      </div>
+<div className="bg-[#2f339d] p-[20px] flex flex-column">
+        
+      </div>
+      </div>
+<div className="bg-[#3fa695] p-[20px] w-[456px] h-[390px] flex flex-row">
+        <div className="bg-[#f0f0f0] p-[20px] w-[344px] h-[67px] flex flex-column">
+        
+      </div>
+<div className="bg-[#177915] p-[20px] w-[54px] h-[233px] flex flex-column">
+        
+      </div>
+      </div>
+      </div>
+<div className="bg-[#c21414] p-[34px] h-[230px] flex flex-column">
+        
+      </div>
+          </>
+        );
+      };
+
+      export default GeneratedPage;
+    
